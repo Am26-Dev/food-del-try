d8a991313783e6f65eb261e2880e54cf7dee62d8
Am26-Dev <inductance2647@gmail.com>
2024-11-06 23:24:29 +0530

    admin nav logo

--- admin/src/components/Navbar/navbar.jsx
+++ admin/src/components/Navbar/navbar.jsx
@@ -1,10 +1,9 @@
-import { assets } from "../../assets/assets";
 import "./navbar.css";
 
 export const Navbar = () =>{
     return(
         <div className="navbar">
-            <img className="logo" src={assets.logo} />
+            <img className="logo" src="/logo.jpg" />
             <i className="profile fa-regular fa-user"></i>
         </div>
     )
